92ee876d7d734b68912429bf1d6c411ee7b93fae
verygoodtuna <133613555+verygoodtuna@users.noreply.github.com>
2023-06-09 14:29:34 +0900

    Add files via upload

--- project_nintendo-main/js/main.js
+++ project_nintendo-main/js/main.js
@@ -12,13 +12,4 @@ $(function () {
     $('.slide_btn .next').on('click', function () {
         $('.m_slide').slick('slickNext')
     })
-
-    $('.family_site span').on('click', function () {
-        $(this).toggleClass('on');
-        $(this).next().toggleClass('on');
-    })
-
-    $('.up_btn').on('click', function(){
-        $('html, body').animate({scrollTop: 0 }, 600)
-    });
 })
